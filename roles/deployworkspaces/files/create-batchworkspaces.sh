28ebaa8028d9acdf5691abdaab3331daccbf7684
svc-ansible-dnc <svc.ansible.dnc@gmail.com>
2019-07-22 13:35:09 -0600

    change bash section

--- roles/deployworkspaces/files/create-batchworkspaces.sh
+++ roles/deployworkspaces/files/create-batchworkspaces.sh
@@ -4,7 +4,7 @@ counter=0
 for i in $(ls payload/*.json);
 do
   ((counter++))
-  aws workspaces create-workspaces --cli-input-json  file://$i
+  aws workspaces create-workspaces --cli-input-json  file://$i --region us-east-1
   if [ $counter -gt 3 ]
   then 
     counter=0
